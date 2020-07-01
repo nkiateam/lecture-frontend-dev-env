8945e1ffc66d861749b23fe7f57367eee5b98ae9
gonghojin <yolip51@naver.com>
2020-07-01 14:42:08 +0900

    ㄱㅛ육 준비 완료

--- src/views/KeywordView.js
+++ src/views/KeywordView.js
@@ -1,5 +1,6 @@
 import View from "./View.js";
 import "./KeywordView.scss";
+import success from "../images/success.png";
 
 export default class KeywordView extends View {
   constructor(el) {
@@ -14,8 +15,8 @@ export default class KeywordView extends View {
 
   mount(data = []) {
     this.el.innerHTML = data.length
-      ? this.getKeywordsHtml(data)
-      : this._messages.NO_KEYWORDS;
+        ? this.getKeywordsHtml(data)
+        : this._messages.NO_KEYWORDS;
     this.show();
     this._bindClickEvent();
     return this;
@@ -23,12 +24,17 @@ export default class KeywordView extends View {
 
   getKeywordsHtml(data) {
     return (
-      data.reduce((html, item, index) => {
-        html += `<li data-keyword="${
-          item.keyword
-        }"><span class="number">${index + 1}</span>${item.keyword}</li>`;
-        return html;
-      }, '<ul class="KeywordView">') + "</ul>"
+        data.reduce((html, item, index) => {
+          if (data.length === index + 1) {
+            html += `<p class="testAction">NKIA 빌드 환경 교육 완료!!(해당 화면이 보인다면 손들어 주세요!!)</p>`;
+            html += `<img src="${success}"/>`;
+            return html;
+          }
+          html += `<li data-keyword="${
+              item.keyword
+          }"><span class="number">${index + 1}</span>${item.keyword}</li>`;
+          return html;
+        }, '<ul class="KeywordView">') + "</ul>"
     );
   }
 
